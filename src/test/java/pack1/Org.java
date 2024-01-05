c1730375997a49b76d58811e9519e32468d3955d
sselvi124 <sselvi124@gmail.com>
2024-01-05 20:37:13 +0530

    one more update

--- src/test/java/pack1/Org.java
+++ src/test/java/pack1/Org.java
@@ -4,5 +4,6 @@ public class Org {
 public static void main(String[] args) {
 	System.out.println("org created");
 	System.out.println("updated success");
+	System.out.println("one more update");
 }
 }
